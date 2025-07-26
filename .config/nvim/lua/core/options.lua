ade18661691a35751f25c85b67daf61ea87c24ba
ramtinJ <ramme95@gmail.com>
2025-07-26 09:20:47 +0200

    removed annoying nvim copy to system clipboard

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,6 +1,6 @@
 vim.wo.number = true -- Make line numbers default (default: false)
 vim.o.relativenumber = true -- Set relative numbered lines (default: false)
-vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
+-- vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
 vim.o.mouse = "a" -- Enable mouse mode (default: '')
 vim.o.autoindent = true -- Copy indent from current line when starting new one (default: true)
 vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
