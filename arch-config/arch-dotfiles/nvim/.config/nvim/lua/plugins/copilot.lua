dc2f853a57fe88fd2df008daeb165f12d9cdd188
Ramtin Javanmardi <ramme95@gmail.com>
2025-10-18 09:52:10 +0200

    Disabling copilot by default, but i want it so not uninstalled

---
+++ arch-config/arch-dotfiles/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        auto_trigger = false,
+        enabled = false,
+      },
+      panel = {
+        enabled = false,
+      },
+    },
+  },
+}
