3a0050da962ad61b32aa35c1041ad28373c683ec
Ramtin Javanmardi <ramme95@gmail.com>
2025-12-25 11:10:41 +0100

    updated test dbt-lsp configs

--- arch-config/arch-dotfiles/nvim/.config/nvim/lua/plugins/dbt_lsp_test.lua
+++ arch-config/arch-dotfiles/nvim/.config/nvim/lua/plugins/dbt_lsp_test.lua
@@ -1,22 +1,21 @@
 return {
-  dir = vim.fn.stdpath("config"),
-  name = "dbt-lsp-manual",
-  config = function()
-    local client = vim.lsp.start_client({
-      name = "dbt-lsp",
-      cmd = { "/home/ramtinj/personal-workspace/dbt-lsp/main" },
-    })
-
-    if not client then
-      vim.notify("hey, you fked up the client thing")
-      return
-    end
-
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "markdown",
-      callback = function()
-        vim.lsp.buf_attach_client(0, client)
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ["dbt_lsp"] = {
+        cmd = { "/home/ramtinj/personal-workspace/dbt-lsp/main" },
+        filetypes = { "markdown" },
+        root_dir = function(bufnr, on_dir)
+          on_dir(vim.fn.getcwd())
+        end,
+      },
+    },
+    setup = {
+      ["dbt_lsp"] = function(server, opts)
+        vim.lsp.config(server, opts)
+        vim.lsp.enable(server)
+        return true
       end,
-    })
-  end,
+    },
+  },
 }
