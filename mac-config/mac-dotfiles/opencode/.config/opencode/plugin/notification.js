ac97b88a1963cd5c22b904596900c21281ff53dc
ramtinJ <ramme95@gmail.com>
2026-02-07 18:15:26 +0100

    updating a bunch of mac configs to make it useable

--- mac-config/mac-dotfiles/opencode/.config/opencode/plugin/notification.js
+++ mac-config/mac-dotfiles/opencode/.config/opencode/plugin/notification.js
@@ -2,7 +2,7 @@ export const NotificationPlugin = async ({ project, client, $, directory, worktr
   return {
     event: async ({ event }) => {
       if (event.type === "session.idle") {
-        await $`notify-send "opencode" "Session completed!"`
+        await $`osascript -e 'display notification "Session completed!" with title "opencode"'`
       }
     },
   }
