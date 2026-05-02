43ce10665bb078fedf24d1b70a1a5eaf2fa39a9f
ramtinJ <ramme95@gmail.com>
2026-05-02 15:41:11 +0200

    fix(dotfiles): clean stale config edges

--- arch-config/arch-dotfiles/nvim/.config/nvim/lua/plugins/theme.lua
+++ arch-config/arch-dotfiles/nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,7 @@
-/home/ramtinj/.config/omarchy/current/theme/neovim.lua
+local omarchy_theme = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
+
+if vim.uv.fs_stat(omarchy_theme) then
+  return dofile(omarchy_theme)
+end
+
+return {}
